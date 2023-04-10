436823f7de812b6bf819a201dd7dc69243f1be8f
Genteki <zhangkaiyuan.null@gmail.com>
2023-04-10 17:08:34 -0400

    fix lint problem

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 """A python library for interacting with the VTube Studio API"""
 
 from setuptools import setup, find_packages
@@ -7,26 +6,29 @@ import pyvts
 DESCRIPTION = "A python library for interacting with the VTube Studio API"
 VERSION = pyvts.__version__
 
-setup(name='pyvts',
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=open("README.md", encoding="utf-8").read(),
-      long_description_content_type='text/markdown',
-      keywords='vtubestudio',
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: Microsoft :: Windows',
-                   'Operating System :: MacOS',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9',
-                   'Programming Language :: Python :: 3.10'],
-      author='Genteki Zhang',
-      author_email='zhangkaiyuan.null@gmail.com',
-      url='https://github.com/Genteki/pyvts',
-      license='MIT',
-      packages=find_packages('pyvts', exclude=["*.tests", "*.test.*"]),
-      install_requires=['websockets', 
-                        'aiofile'])
+setup(
+    name="pyvts",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="vtubestudio",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    author="Genteki Zhang",
+    author_email="zhangkaiyuan.null@gmail.com",
+    url="https://github.com/Genteki/pyvts",
+    license="MIT",
+    packages=find_packages("pyvts", exclude=["*.tests", "*.test.*"]),
+    install_requires=["websockets", "aiofile"],
+)
